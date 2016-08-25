c3fdfbce9d0d8b641659d6025890839bb0746602
Miroslav Vidovic <miroslav-vidovic@hotmail.com>
2016-08-25 12:43:10 +0200

    Fixed slow loading time

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,7 +34,7 @@ jQuery(document).ready(function($) {
         // how many entries do you want?
         // default: 4
         // valid values: any integer
-        limit: 16,
+        limit: 18,
 
         // the effect, which is used to let the entries appear
         // default: 'show'
